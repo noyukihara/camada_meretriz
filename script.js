4a14cb5896ea5c491ff71c7425d6859b8e3fd80a
noyukihara <noriyuki95.at@gmail.com>
2014-06-05 18:48:49 -0300

    failed button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-	
+$(document).ready(function(){	
 	var isBreakPoint = function (bp) {
     	var bps = [320, 480, 768, 1024],
         	w = $(window).width(),
@@ -13,7 +12,6 @@ $(document).ready(function(){
    			 };
    		return w > min && w <= max
 	};
-
 	// Usage
 	if (isBreakPoint(480)) {
 		$('.mobSlider').bxSlider({
@@ -21,109 +19,23 @@ $(document).ready(function(){
 	    		minSlides: 1,
 	    		maxSlides: 1,
   			});	
-	} // Breakpoint between 320 and 480
+	}; // Breakpoint between 320 and 480
 
 	/*Fancybox - Configurações*/
 	 $('a.fancybox').fancybox();
 	 $(".fancybox").fancybox({
 		 	padding: 0,
 		 	overlayOpacity: 0.6,
-		 });
-	
-	/*$(window).resize(function(){
-
-       if ($(window).width() <= 770) {  
-       		
-       }     
-	});*/
-
-	/*var flor = $( ".flor" );
-	var position = flor.position();
-
-	var width = $(window).width;*/
-		
+	 });	
+			
 	var hide= function(id){
 		$(id).css("display", "none");
 	};
 
 	var show= function(id){
 		$(id).css("display", "block");
-	};
+	};	
 
-	var florEsquerda=function(){
-		if (flor == 1) {
-				$(".flor").click(function(){
-					show("#transforme");
-					$("#second").animate({width:"100%"},{"duration": 1000}, function(){
-						$("#first").css("display", "none");
-						flor=2;
-					});
-					$("#first").animate({width:"0%"},{"duration": 1000});
-					hide("#conheca");
-					$(".flor").animate({left: "-50%"}, 1000);
-					hide(".first");
-					show(".second");
-					show(".p2");
-					hide(".p1");
-
-				});
-			}else if(flor == 2){
-				$(".flor").click(function(){
-					show("#conheca");
-					$("#first").animate({width:"100%"},{"duration": 1000,}, function(){
-						flor=1;
-						$("#second").css("display", "none");
-					});
-					$("#second").animate({width:"0%"},{"duration": 1000});
-					hide("#transforme");
-					$(".flor").animate({left: "+50%"}, 1000);
-					show(".first");
-					hide(".second");
-					show(".p1");
-					hide(".p2")
-				});
-			};
-	};
-
-	var florDireita=function(){
-		if (flor == 1) {
-				$('.flor').click(function(){
-					show("#transforme");
-					$("#second").animate({width:"100%"},{"duration": 1000}, function(){
-						$("#first").css("display", "none");
-						flor=0;
-					});
-					$("#first").animate({width:"0%"},{"duration": 1000});
-					hide("#conheca");
-					$(".flor").animate({left: "-50%"}, 1000);
-					hide(".first");
-					show(".second");
-					show(".p2");
-					hide(".p1");
-
-				});
-			}else if(flor == 2){
-				$('.flor').click(function(){
-					show("#conheca");
-					$("#first").animate({width:"100%"},{"duration": 1000,}, function(){
-						flor=0;
-						$("#second").css("display", "none");
-					});
-					$("#second").animate({width:"0%"},{"duration": 1000});
-					hide("#transforme");
-					$(".flor").animate({left: "+50%"}, 1000);
-					show(".first");
-					hide(".second");
-					show(".p1");
-					hide(".p2")
-				});
-			};
-	}
-
-	var flor;
-	var flor=0;
-
-	
 	/*Função de Abaixar Rápido*/
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -138,134 +50,93 @@ $(document).ready(function(){
 	    });
 	});
 	
-	if ($(window).width() >= 1024 ) {
-		hide(".p1");
-		hide(".p2"); 
-		$("body").css("overflow", "hidden"); 
-		
-		$('#first').click(function(){
-			show("#conheca");
-				$(this).animate({width:"100%"},{"duration": 1000,}, function(){
-					$("#second").css("display", "none");
-				});
-			$("#second").animate({width:"0%"},{"duration": 1000});
-			hide("#transforme");
-			$(".flor").animate({left: "+50%"}, 1000);
-			$(".flor").css("cursor", "pointer");			
-			
-			show(".first");
-			hide(".second");
-			show(".p1");
-			hide(".p2")
-			flor=1;
-			$("body").css("overflow", "auto"); 
-			$('html, body').animate({scrollTop : 0},800);
-
-			florEsquerda();
-		});
-
-		$('#second').click(function(){
+	/*clicked= !clicked;
+	if (clicked==true){
+		$('.flor').click(function(){
 			show("#transforme");
-				$(this).animate({width:"100%"},{"duration": 1000}, function(){
-					$("#first").css("display", "none");
-				});
+			$("#second").animate({width:"100%"},{"duration": 1000}, function(){
+				$("#first").css("display", "none");
+			});
 			$("#first").animate({width:"0%"},{"duration": 1000});
 			hide("#conheca");
 			$(".flor").animate({left: "-50%"}, 1000);
-			$(".flor").css("cursor", "pointer");
 			hide(".first");
 			show(".second");
 			show(".p2");
 			hide(".p1");
-			flor=2;
-			$("body").css("overflow", "auto");		
-			$('html, body').animate({scrollTop : 0},800);
-
-			florDireita();
 		});
-			
-		$('#logo').click(function(){
-			$('html, body').animate({scrollTop : 0},800);
-			$("#first").animate({width:"50%"},{"duration": 1000});
-			$("#second").animate({width:"50%"},{"duration": 1000});
-			show("h1");
-			$(".flor").animate({left: "0%"},{"duration": 1000});
-			hide(".first");
-			hide(".second");
-			hide(".p1");
-			hide(".p2");
-			flor=0;
-			$(".flor").css("cursor", "initial");
-			$("body").css("overflow", "hidden");
-
-			$('html, body').animate({scrollTop : 0},800); 
-		});
-
-			/*if (flor == 1) {
-				$('.flor').click(function(){
-					show("#transforme");
-					$("#second").animate({width:"100%"},{"duration": 1000}, function(){
-						$("#first").css("display", "none");
-						flor=2;
-					});
-					$("#first").animate({width:"0%"},{"duration": 1000});
-					hide("#conheca");
-					$(".flor").animate({left: "-50%"}, 1000);
-					hide(".first");
-					show(".second");
-					show(".p2");
-					hide(".p1");
-
-				});
-			}else if(flor == 2){
-				$('.flor').click(function(){
-					show("#conheca");
-					$("#first").animate({width:"100%"},{"duration": 1000,}, function(){
-						flor=1;
-						$("#second").css("display", "none");
-					});
-					$("#second").animate({width:"0%"},{"duration": 1000});
-					hide("#transforme");
-					$(".flor").animate({left: "+50%"}, 1000);
-					show(".first");
-					hide(".second");
-					show(".p1");
-					hide(".p2")
-				});
-			};*/
-
-		/*if(position<=width){
-				$('.flor').click(function(){
-					show("#transforme");
-					$("#second").animate({width:"100%"},{"duration": 1000}, function(){
-						$("#first").css("display", "none");
-					});
-					$("#first").animate({width:"0%"},{"duration": 1000});
-					hide("#conheca");
-					$(".flor").animate({left: "-50%"}, 1000);
-					hide(".first");
-					show(".second");
-					show(".p2");
-					hide(".p1");
-				});
-			}
-			
-			else{
-				$('.flor').click(function(){
-					show("#conheca");
-					$("#first").animate({width:"100%"},{"duration": 1000,}, function(){
+		}else{
+			$('.flor').click(function(){
+				show("#conheca");
+				$("#first").animate({width:"100%"},{"duration": 1000,}, function(){
 					$("#second").css("display", "none");
-					});
-					$("#second").animate({width:"0%"},{"duration": 1000});
-					hide("#transforme");
-					$(".flor").animate({left: "+50%"}, 1000);
-					show(".first");
-					hide(".second");
-					show(".p1");
-					hide(".p2")
+				});
+				$("#second").animate({width:"0%"},{"duration": 1000});
+				hide("#transforme");
+				$(".flor").animate({left: "+50%"}, 1000);
+				show(".first");
+				hide(".second");
+				show(".p1");
+				hide(".p2");
 			});
 		};*/
-	};
-});			
+	
+		if ($(window).width() >= 1024 ) {
+			$("body").css("overflow", "hidden"); 
+			hide(".p1");
+			hide(".p2"); 
+						
+			$('#first').click(function(){
+				show("#conheca");
+					$(this).animate({width:"100%"},{"duration": 1000,}, function(){
+						$("#second").css("display", "none");
+					});
+				$("#second").animate({width:"0%"},{"duration": 1000});
+				hide("#transforme");
+				$(".flor").animate({left: "+50%"}, 1000);
+				$(".flor").css("cursor", "pointer");			
+				
+				show(".first");
+				hide(".second");
+				show(".p1");
+				hide(".p2")
+				$("body").css("overflow", "auto"); 
+				$('html, body').animate({scrollTop : 0},800);
+			});
 
+			$('#second').click(function(){
+				show("#transforme");
+					$(this).animate({width:"100%"},{"duration": 1000}, function(){
+						$("#first").css("display", "none");
+					});
+				$("#first").animate({width:"0%"},{"duration": 1000});
+				hide("#conheca");
+				$(".flor").animate({left: "-50%"}, 1000);
+				$(".flor").css("cursor", "pointer");
+				hide(".first");
+				show(".second");
+				show(".p2");
+				hide(".p1");
+				$("body").css("overflow", "auto");		
+				$('html, body').animate({scrollTop : 0},800);
+			});
+				
+			$('#logo').click(function(){
+				$('html, body').animate({scrollTop : 0},800);
+				$("#first").animate({width:"50%"},{"duration": 1000});
+				$("#second").animate({width:"50%"},{"duration": 1000});
+				show("h1");
+				$(".flor").animate({left: "0%"},{"duration": 1000});
+				hide(".first");
+				hide(".second");
+				hide(".p1");
+				hide(".p2");
+				$(".flor").css("cursor", "initial");
+				$("body").css("overflow", "hidden");
 
+				$('html, body').animate({scrollTop : 0},800); 
+			});
+
+			
+			};
+		});
